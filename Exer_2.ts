dec3d6a986a3c317aeb32c136d5774af4f974f7e
Lázaro Enrico <97999169+Lazaroenrico@users.noreply.github.com>
2022-05-23 19:56:32 -0300

    Exercícios-semana 1

---
+++ Exer_2.ts
@@ -0,0 +1,48 @@
+interface User {
+    name: string,
+    age: number,
+    occupation: string,
+    role?: never,
+}
+
+interface Admin {
+    
+    name: string,
+    age: number,
+    occupation?: never,
+    role: string,
+
+}
+
+// Em vez de colocar never ( em occupation e no role) poderiamos utilizar o string ou o any.
+
+export type Person = User | Admin;
+
+export const persons: Person[] = [
+    {
+        name: 'Max Mustermann',
+        age: 25,
+        occupation: 'Chimney sweep'
+    },
+    {
+        name: 'Jane Doe',
+        age: 32,
+        role: 'Administrator'
+    },
+    {
+        name: 'Kate Müller',
+        age: 23,
+        occupation: 'Astronaut'
+    },
+    {
+        name: 'Bruce Willis',
+        age: 64,
+        role: 'World saver'
+    }
+];
+
+export function logPerson(user: Person) {
+    console.log(` - ${user.name}, ${user.age}`);
+}
+
+persons.forEach(logPerson);
